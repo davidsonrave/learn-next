dd0c278a9fed39ce02dde46a48a15745e51dccfa
Davidson Rave <drave@plm.com.co>
2024-08-26 11:17:16 -0500

    first commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -21,7 +21,6 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
-
 export default function NavLinks() {
   const pathname = usePathname();
   return (
